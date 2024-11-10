71c7afb67030fd8ca883d8ff09b29392c4204798
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2024-11-10 23:26:51 +0530

    learnings

---
+++ patterns/numberpattern2.py
@@ -0,0 +1,12 @@
+# 55555
+# 4444
+# 333
+# 22
+# 1
+
+n = int(input("enter"))
+
+for i in range(n):
+    for j in range(n-i):
+        print(n-i,end=" ")
+    print("")
---
+++ patterns/numberpattern4.py
@@ -0,0 +1,13 @@
+# 1 2 3 4 5
+# 1 2 3 4
+# 1 2 3
+# 1 2
+# 1
+
+n = int(input("Enter"))
+
+for i in range(n):
+    for j in range(1,n-i+1):
+        print(j,end=" ")
+        # print(n-,end="")
+    print("")
